de0f9de7f8ffe9b06408ca4678a2665967d5398b
rok <roky.zz@gmail.com>
2018-03-24 02:02:02 +0100

    component prepared for receiving and displaying data

--- public/components/Content.jsx
+++ public/components/Content.jsx
@@ -8,9 +8,21 @@ import { LOGIN, LOGOUT } from "StateVariables";
 export class Content extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = { input: ""};
+		this._handleSearch = this._handleSearch.bind(this);
+		this._handleInput = this._handleInput.bind(this);
+	}
+
+	_handleSearch() {
+		this.props.getBars(this.state.input);
+	}
+
+	_handleInput(e) {
+		this.setState({ input: e.target.value });
 	}
 
 	render() {
+		
 		return (
 			<section className="content">
 				<img className="content__hero" srcSet="../public/assets/images/pexels-photo-260920.jpeg 640w, ../public/assets/images/pexels-photo-260921.jpeg 1280w, ../public/assets/images/pexels-photo-260922.jpeg 1920w" alt="barcle"/>
@@ -22,10 +34,17 @@ export class Content extends React.Component {
 						<i className="fa fa-coffee content__main__img-container__img"></i>
 						<i className="fa fa-beer content__main__img-container__img"></i>
 					</div>
-					<input className="content__main__searchbar" type="text" size="50" placeholder="Where are you at?"/>
-					<button className="content__main__search-button">Search</button>
+					<input className="content__main__searchbar" onChange={this._handleInput} type="text" size="50" placeholder="Where are you at?"/>
+					<button className="content__main__search-button" onClick={this._handleSearch}>Search</button>
 				</article>
+				{Object.keys(this.props.bars).map( (key, index) => <article key={index} className="content__results">{this.props.bars[key]}</article>)}
 			</section>
 		);
 	}
 }
+
+Content.propTypes = {
+
+	getBars: PropTypes.func,
+	bars: PropTypes.object
+};
