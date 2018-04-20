cd38abd766e32e7cedfe68c92e1e8ad7034d6636
rok <roky.zz@gmail.com>
2018-04-20 12:53:14 +0200

    gitignore dist files + redux devtools store supplement

--- public/index.jsx
+++ public/index.jsx
@@ -14,9 +14,9 @@ import rootReducer from "RootReducer";
 import Home from "App";
 import css from "./style/index";
 
-
+/* eslint-disable no-underscore-dangle */
 ReactDOM.render(
-	<Provider store={createStore(rootReducer, applyMiddleware(thunkMiddleware, loggerMiddleware))}>
+	<Provider store={createStore(rootReducer, applyMiddleware(thunkMiddleware, loggerMiddleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
 		<Router>
 			<Switch>
 				<Route exact path="/" component={Home} />
@@ -25,4 +25,4 @@ ReactDOM.render(
 		</Router>
 	</Provider>,
 	document.getElementById("root")
-);
+);/* eslint-enable */
