36c95eb1e4d2d3208e1e2a24c965423981289efd
rok <roky.zz@gmail.com>
2018-05-30 20:17:39 +0200

    Card component - minor changes in style transitions

--- public/components/home/_Card.jsx
+++ public/components/home/_Card.jsx
@@ -29,7 +29,7 @@ export default class Card extends React.Component {
 		new Waypoint({
 			element,
 			handler: () => element.setAttribute("class", "content__cards__card reveal-item--is-visible"),
-			offset: "65%"
+			offset: "60%"
 		});
 	}
 
